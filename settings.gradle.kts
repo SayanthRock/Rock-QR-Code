84f3bc37a9e062be413082e402503e9d86181356
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-06-22 23:29:27 +0530

    reset settings gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,5 @@
-pluginManagement {
-  repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("androidx.*")
-      }
-    }
-    mavenCentral()
-    gradlePluginPortal()
-  }
-}
-
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
-
-dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
-}
-
-rootProject.name = "ROCK Qr"
-
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "My Application"
 include(":app")
